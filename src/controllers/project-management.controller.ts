370d6819889501ab99bac47014ebcd1beff92bcb
Ojvar <MOjvar.AmirHossein@gmail.com>
2024-06-11 14:19:07 +0330

    update: add guard to project-management controller

--- src/controllers/project-management.controller.ts
+++ src/controllers/project-management.controller.ts
@@ -1,18 +1,20 @@
-import {inject} from '@loopback/context';
+import {inject, intercept} from '@loopback/context';
 import {ProjectManagementService} from '../services';
-import {get, getModelSchemaRef, param} from '@loopback/rest';
+import {getModelSchemaRef, param, post} from '@loopback/rest';
 import {ProjectRegistrationCodeDTO, ProjectSummaryEngineerDTO} from '../dto';
+import {EnumRoles, protect} from '../lib-keycloak/src';
 
 const BASE_ADDR = '/project-management';
 const tags = ['ProjectManagement'];
 
+@intercept(protect(EnumRoles.PROJECTS_SERVIE_MANAGER))
 export class ProjectManagementController {
   constructor(
     @inject(ProjectManagementService.BINDING_KEY)
     private projectMangementService: ProjectManagementService,
   ) {}
 
-  @get(`${BASE_ADDR}/{n_id}`, {
+  @post(`${BASE_ADDR}/{n_id}`, {
     tags,
     summary: 'Get validation code to registrating new project',
     description: 'Get validation code to registrating new project',
